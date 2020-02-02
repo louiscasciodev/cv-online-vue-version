ad030d9ed326a89c83e7c2720015eae652a460f7
dolfant <louis.cascio@hotmail.fr>
2020-02-02 14:43:49 +0100

    feat(search): vuex store init

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,87 +1,85 @@
 <template>
-  <div class="home">
-    <div class="page-container">
-      <md-app>
-        <md-app-toolbar class="md-primary md-medium" md-elevation="5">
-          <div class="start-search-container">
-            <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
-              <md-icon>menu</md-icon>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary md-medium" md-elevation="5">
+        <div class="start-search-container">
+          <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
+          <span class="md-title">CV en ligne</span>
+        </div>
+        <div class="search-container">
+          <md-autocomplete
+            class="search"
+            v-model="selectedRepo"
+            :md-options="reposList"
+            :md-open-on-focus="false"
+            md-layout="box"
+          >
+            <label>Search...</label>
+          </md-autocomplete>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <div class="md-toolbar-section-end">
+            <md-avatar class="md-elevation-1">
+              <img src="https://avatars1.githubusercontent.com/u/55145958?v=4" alt="Avatar" />
+            </md-avatar>
+            <span class="md-body-2">Louis Cascio</span>
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
             </md-button>
-            <span class="md-title">CV en ligne</span>
           </div>
-          <div class="search-container">
-            <md-autocomplete
-              class="search"
-              v-model="selectedRepo"
-              :md-options="reposList"
-              :md-open-on-focus="false"
-              md-layout="box"
-            >
-              <label>Search...</label>
-            </md-autocomplete>
-          </div>
-        </md-app-toolbar>
+        </md-toolbar>
 
-        <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
-          <md-toolbar class="md-transparent" md-elevation="0">
-            <div class="md-toolbar-section-end">
-              <md-avatar class="md-elevation-1">
-                <img src="https://avatars1.githubusercontent.com/u/55145958?v=4" alt="Avatar" />
-              </md-avatar>
-              <span class="md-body-2">Louis Cascio</span>
-              <md-button class="md-icon-button md-dense" @click="toggleMenu">
-                <md-icon>keyboard_arrow_left</md-icon>
-              </md-button>
-            </div>
-          </md-toolbar>
+        <md-list>
+          <md-subheader class="md-inset">Projets</md-subheader>
 
-          <md-list>
-            <md-subheader class="md-inset">Projets</md-subheader>
+          <md-divider class="md-inset"></md-divider>
 
-            <md-divider class="md-inset"></md-divider>
+          <md-list-item to="/project/all" exact>
+            <md-icon>list</md-icon>
+            <span class="md-list-item-text">Tous</span>
+          </md-list-item>
 
-            <md-list-item to="/project/all" exact>
-              <md-icon>list</md-icon>
-              <span class="md-list-item-text">Tous</span>
-            </md-list-item>
+          <md-list-item to="/project/3" exact>
+            <md-icon>filter_3</md-icon>
+            <span class="md-list-item-text">Projet 3</span>
+          </md-list-item>
 
-            <md-list-item to="/project/3" exact>
-              <md-icon>filter_3</md-icon>
-              <span class="md-list-item-text">Projet 3</span>
-            </md-list-item>
+          <md-list-item to="/project/2" exact>
+            <md-icon>filter_2</md-icon>
+            <span class="md-list-item-text">Projet 2</span>
+          </md-list-item>
 
-            <md-list-item to="/project/2" exact>
-              <md-icon>filter_2</md-icon>
-              <span class="md-list-item-text">Projet 2</span>
-            </md-list-item>
+          <md-list-item to="/project/1" exact>
+            <md-icon>filter_1</md-icon>
+            <span class="md-list-item-text">Projet 1</span>
+          </md-list-item>
 
-            <md-list-item to="/project/1" exact>
-              <md-icon>filter_1</md-icon>
-              <span class="md-list-item-text">Projet 1</span>
-            </md-list-item>
+          <md-subheader class="md-inset">Téléchargements</md-subheader>
 
-            <md-subheader class="md-inset">Téléchargements</md-subheader>
+          <md-divider class="md-inset"></md-divider>
 
-            <md-divider class="md-inset"></md-divider>
-
-            <md-list-item to="/cv" exact>
-              <md-icon>attachment</md-icon>
-              <span class="md-list-item-text">Curriculum vitæ</span>
-            </md-list-item>
-          </md-list>
-        </md-app-drawer>
-        <md-app-content>
-          <router-link to="/project/all"></router-link>
-          <router-link to="/project/3"></router-link>
-          <router-link to="/project/2"></router-link>
-          <router-link to="/project/1"></router-link>
-          <router-link to="/cv"></router-link>
-          <transition name="slide-fade">
-            <router-view></router-view>
-          </transition>
-        </md-app-content>
-      </md-app>
-    </div>
+          <md-list-item to="/cv" exact>
+            <md-icon>attachment</md-icon>
+            <span class="md-list-item-text">Curriculum vitæ</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+      <md-app-content>
+        <router-link to="/project/all"></router-link>
+        <router-link to="/project/3"></router-link>
+        <router-link to="/project/2"></router-link>
+        <router-link to="/project/1"></router-link>
+        <router-link to="/cv"></router-link>
+        <transition name="slide-fade">
+          <router-view></router-view>
+        </transition>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
@@ -102,7 +100,7 @@ export default {
   computed: {
     selectedRepo: {
       get: function() {
-        return this.$store.state.selectedRepo
+        return this.$store.state.selectedRepo;
       },
       set: function(newValue) {
         this.$store.dispatch("setSelectedRepo", newValue);
