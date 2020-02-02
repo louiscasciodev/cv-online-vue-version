80b05ac993da17f772c1296132348b1b9df6dccb
dolfant <louis.cascio@hotmail.fr>
2020-02-02 15:44:54 +0100

    search child

---
+++ src/views/TheHomeSearch.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="search-container">
+    <md-autocomplete
+      class="search"
+      v-model="selectedRepo"
+      :md-options="reposList"
+      :md-open-on-focus="false"
+      md-layout="box"
+    >
+      <label>Search...</label>
+    </md-autocomplete>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "TheHomeSearch",
+  data: function() {
+    return {
+      dataList: [],
+      reposList: [],
+      loaded: false,
+      errored: false
+    };
+  },
+  computed: {
+    selectedRepo: {
+      get: function() {
+        return this.$store.state.selectedRepo;
+      },
+      set: function(newValue) {
+        this.$store.dispatch("setSelectedRepo", newValue);
+        //nom de l'action + newValue
+      }
+    }
+  },
+  methods: {
+    async getData() {
+      await axios
+        .get("https://api.github.com/users/louiscasciodev/repos")
+        .then(response => {
+          this.dataList = response.data;
+        });
+      this.getReposList();
+    },
+    getReposList() {
+      this.dataList.map(repo => {
+        return this.reposList.push(repo.name);
+      });
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+};
+</script>
+
+<style scoped>
+.start-search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.search-container {
+  width: 80%;
+  display: flex;
+  justify-content: center;
+}
+
+.search {
+  max-width: 500px;
+}
+</style>
